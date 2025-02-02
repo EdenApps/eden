2e07ae68c52d966c5860ca6b9699565ed65c3593
Justin Donnaruma <justin@donnaruma.us>
2025-02-02 14:15:53 -0500

    Remove project_hr_skills module - incorect license.

--- addons/project_hr_skills/models/project_task.py
+++
@@ -1,8 +0,0 @@
-# Part of Eden. See LICENSE file for full copyright and licensing details.
-
-from eden import fields, models
-
-class ProjectTask(models.Model):
-    _inherit = "project.task"
-
-    user_skill_ids = fields.One2many('hr.employee.skill', related='user_ids.employee_skill_ids')
