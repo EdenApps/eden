6f82796e7bfca43b0d1e97b5791b6009840aba86
Justin Donnaruma <Justin@Donnaruma.us>
2025-03-10 14:51:34 -0400

    Add updates from MUK as defaults. TODO: Incorporate into Core.

---
+++ addons/muk_web_appsbar/models/res_config_settings.py
@@ -0,0 +1,15 @@
+from eden import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    #----------------------------------------------------------
+    # Fields
+    #----------------------------------------------------------
+    
+    appbar_image = fields.Binary(
+        related='company_id.appbar_image',
+        readonly=False
+    )
---
+++ addons/muk_web_utils/models/ir_http.py
@@ -0,0 +1,22 @@
+from eden import models
+from eden.http import request
+from eden.tools import str2bool
+
+
+class IrHttp(models.AbstractModel):
+
+    _inherit = 'ir.http'
+
+    #----------------------------------------------------------
+    # Functions
+    #----------------------------------------------------------
+    
+    def session_info(self):
+        result = super(IrHttp, self).session_info()
+        result['disable_quick_create'] = str2bool(
+            self.env['ir.config_parameter'].sudo().get_param(
+                'muk_web_utils.disable_quick_create', default=''
+            ),
+            default=False
+        )
+        return result
